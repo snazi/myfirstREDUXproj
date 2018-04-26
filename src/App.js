9ae391f5c332643d5777495af4b2805d1fc5f06d
Angelo Amadora <amadora.angelo@gmail.com>
2018-04-26 17:12:52 +0800

    can use the state now

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import {View} from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import {Header} from 'react-native-elements';
+import LibraryList from './components/LibraryList';
 
 const App = () => {
     return (
         <Provider store={ createStore(reducers) }>
-            <View />
+        <View>
+            <Header
+                leftComponent={{ icon: 'menu', color: '#fff' }}
+                centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+                rightComponent={{ icon: 'home', color: '#fff' }}
+                />
+
+            <LibraryList />
+        </View>
         </Provider>
     );
-}
+};
 
 export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import LibraryReducer from './LibraryReducer';
 
 export default combineReducers({
 
-    libraries: () => []
+    libraries: LibraryReducer
     
 });
